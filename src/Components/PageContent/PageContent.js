452ce3f4f9c1d485fd26a5d572315b0e42d6a077
Brandon Clark <bwclark97@gmail.com>
2020-06-04 19:20:16 -0400

    Base layout set!
    Co-authored-by: Boyd Duffie V <shmeeves@ufl.edu>

--- src/Components/PageContent/PageContent.js
+++ src/Components/PageContent/PageContent.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./PageContent.css";
-import {Player} from "../Player/Player"
+import {Player} from "../Player/Player";
+import {RightPlayer} from "../RightPlayer/RightPlayer";
 import { animated } from 'react-spring';
 import { About } from "../About/About";
-import opus1 from '../Player/opus1.png'
+import opus1 from '../Player/opus1.png';
+import billions from '../RightPlayer/billions.png';
 
 
 
@@ -19,30 +21,16 @@ export class PageContent extends React.Component {
         return (
           <div className="Home">
             <h1 className="title">Locals Only Sound</h1>
-            <animated.div className="textone">
+            <animated.div className="player1">
               <center>
                 <Player imagesrc={opus1} />
               </center>
             </animated.div>
-            <div className="texttwo">
-              <p className="texttwo">
-                Loreipsum dolor sit amet, consectetur adipiscing elit. Nunc leo
-                nulla, consequat sed sapien sed, efficitur tincidunt nulla.
-                Integer venenatis metus et odio molestie interdum. Suspendisse
-                mattis porttitor dapibus. Aliquam vehicula tempor odio, at
-                laoreet nisi bibendum quis. Nullam a libero massa. Quisque
-                convallis metus et ligula lacinia, non dictum nisl pellentesque.
-                Mauris sagittis, ipsum at consequat luctus, nisl quam dapibus
-                lorem, a rhoncus erat ipsum nec eros. Sed semper tortor sit amet
-                lorem aliquet sagittis. Vestibulum condimentum bibendum turpis
-                in iaculis. Vivamus sed varius lorem. Fusce congue elementum
-                neque, sed laoreet sapien convallis vel. Nullam tortor ligula,
-                eleifend non semper id, ultrices eu ligula. In sed erat
-                fringilla, porta magna ac, aliquam sem. Aliquam et accumsan
-                metus. In blandit neque vel purus dapibus, ac placerat elit
-                mattis. Proin placerat scelerisque ligula tempus faucibus.
-              </p>
-            </div>
+            <animated.div className="player2">
+              <center>
+                <RightPlayer imagesrc={billions} />
+              </center>
+            </animated.div>
           </div>
         );
       case "TourDates":
--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import { useSpring, animated } from 'react-spring';
-import './Player.css';
-import opus1 from './opus1.png';
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import "./Player.css";
+import opus1 from "./opus1.png";
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, ( x - window.innerWidth /4) / 20, 1.1];
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 4) / 20,
+  1.1,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 export const Player = (props) => {
-    
-    const [animationSet, set] = useSpring(() => ({ xys: [0, 0, 1], config: {mass: 5, tension: 350, friction: 40 }}))
-    
-    return (
-      <animated.div
-        className="hoverPlayer"
-        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-        onMouseLeave={() => set({ xys: [0, 0, 1] })}
-        style={{ 
-                transform: animationSet.xys.interpolate(trans),
-                backgroundImage: `url(${props.imagesrc})`
-        }}
-      />
-    );
-}
+  const [animationSet, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
+  return (
+    <div className="leftcontainer">
+      <center>
+        <animated.div
+          className="hoverPlayer"
+          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+          onMouseLeave={() => set({ xys: [0, 0, 1] })}
+          style={{
+            transform: animationSet.xys.interpolate(trans),
+            backgroundImage: `url(${props.imagesrc})`,
+          }}
+        />
+      </center>
+      <div className="albumText">
+        <p>
+          Loreipsum dolor sit amet, consectetur adipiscing elit. Nunc leo nulla,
+          consequat sed sapien sed, efficitur tincidunt nulla. Integer venenatis
+          metus et odio molestie interdum. Suspendisse mattis porttitor dapibus.
+          Aliquam vehicula tempor odio, at laoreet nisi bibendum quis. Nullam a
+          libero massa. Quisque convallis metus et ligula lacinia, non dictum
+          nisl pellentesque. Mauris sagittis, ipsum at consequat luctus, nisl
+          quam dapibus lorem, a rhoncus erat ipsum nec eros. Sed semper tortor
+          sit amet lorem aliquet sagittis. Vestibulum condimentum bibendum
+          turpis in iaculis. Vivamus sed varius lorem. Fusce congue elementum
+          neque, sed laoreet sapien convallis vel. Nullam tortor ligula,
+          eleifend non semper id, ultrices eu ligula. In sed erat fringilla,
+          porta magna ac, aliquam sem. Aliquam et accumsan metus. In blandit
+          neque vel purus dapibus, ac placerat elit mattis. Proin placerat
+          scelerisque ligula tempus faucibus.
+        </p>
+      </div>
+    </div>
+  );
+};
