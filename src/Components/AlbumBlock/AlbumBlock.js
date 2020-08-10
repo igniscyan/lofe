70e30c7eb52048895ac1d2bba3a4d4d4e413016a
Boyd Duffie V <shmeeves@ufl.edu>
2020-08-10 12:29:58 -0400

    added some new styling to homepage, ready to move into figuring out audio components

--- src/Components/AlbumBlock/AlbumBlock.js
+++ src/Components/AlbumBlock/AlbumBlock.js
@@ -4,7 +4,7 @@ import { Spotify } from '../../util/Spotify';
 import '../../util/localsonly.json';
 import { useSpring, animated } from 'react-spring';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
 // Here is where we might wanna query our JSON of URIs and return specific info based on the name that's being passed as a prop.
 // The props ought to be named the same as the actual individual objects within that JSON file.
@@ -13,6 +13,7 @@ import { faPlay } from '@fortawesome/free-solid-svg-icons';
 export const AlbumBlock = ({ query }) => {
   const [album, setAlbum] = useState({});
   const [hovering, setHovering] = useState(false);
+  const [buttonHovering, setButtonHovering] = useState(false);
 
   useEffect(() => {
     Spotify.getAlbum(query.id).then((res) => setAlbum(res));
@@ -24,13 +25,20 @@ export const AlbumBlock = ({ query }) => {
 
   const buttonSpring = useSpring({
     opacity: hovering ? 1 : 0,
+    transform: buttonHovering ? 'scale(1.2)' : 'scale(1)',
+    cursor: 'pointer',
+  });
+
+  const cornerSpring = useSpring({
+    borderRadius: hovering ? '15px' : '0px',
   });
 
   return (
     <div className="ab-container">
-      <h1 className="ab-header">{`${album.name}`}</h1>
-      <div
-        className="art-background"
+      <h1 className="ab-header">{album.name}</h1>
+      <animated.div
+        className="art-container"
+        style={{ ...cornerSpring }}
         onMouseOver={() => setHovering(true)}
         onMouseOut={() => setHovering(false)}>
         <animated.img
@@ -40,9 +48,13 @@ export const AlbumBlock = ({ query }) => {
           style={{ ...artSpring }}
         />
         <animated.div className="play-button" style={{ ...buttonSpring }}>
-          <FontAwesomeIcon icon={faPlay} />
+          <FontAwesomeIcon
+            icon={faPlayCircle}
+            onMouseOver={() => setButtonHovering(true)}
+            onMouseOut={() => setButtonHovering(false)}
+          />
         </animated.div>
-      </div>
+      </animated.div>
     </div>
   );
 };
