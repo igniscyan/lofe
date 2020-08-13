e245b9a7d9a130a1d24e9be7eeae4e0a932e9357
Boyd Duffie V <shmeeves@ufl.edu>
2020-08-13 17:09:45 -0400

    forgot to remove preloader import...

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 //import logo from '../../../public/logo.png';
 import './App.css';
 import { NavBar } from '../NavBar/NavBar';
-import { Preloader } from '../Preloader/Preloader';
 import { Home } from '../Home/Home';
 import { About } from '../About/About';
 import { Footer } from '../Footer/Footer';
