7c90569114bc4bf7ec23c6bd67d570b65e585f08
Boyd Duffie V <shmeeves@ufl.edu>
2020-08-13 10:00:41 -0400

    clicking the play button plays/pauses the song. Next up is changing the icon when the button is clicked.

--- src/Components/Audio/Audio.js
+++ src/Components/Audio/Audio.js
@@ -1,51 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Audio.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlayCircle as play,
+  faPauseCircle as pause,
+} from '@fortawesome/free-regular-svg-icons';
+import { faSpinner as loading } from '@fortawesome/free-solid-svg-icons';
 
 export const Audio = (props) => {
-    console.log(props.name);
-    return(
-        <div>
-            {console.log(props.url)}
-            <p>{props.name}</p>
-            <video src={props.url} autoPlay></video>
-        </div>
-    );
-}
+  const [video, setVideo] = useState(undefined);
+  const [playing, setPlaying] = useState(false);
 
-/*
-now here is where some fiddling might need to take place bc i'm not all that familiar with the audio html tag
-but from what I understand, we make an audio wrapper and a source tag inside it.
-we're gonna have to specify the file type I thiiink ? Itunes previews are in M4A which is supportedSWAGEPICMONEYGAMERWEED
-I have a feeling that the rest of our task will be fuckin with css then, and trying to get the tracklist to overlay wit
-the album. probably an onHover spring with an opacity thi
+  useEffect(() => {
+    setVideo(document.getElementById(props.song.uri));
+    console.log(video);
+  }, []);
 
+  const togglePlay = (e) => {
+    if (video.paused) {
+      video.play();
+      e.target.icon = pause;
+    } else {
+      video.pause();
+      e.target.icon = play;
+    }
+  };
 
-1. No worries on that for now, we can always get it positioned where we want after we make it work. But I think what we'll
-want it to do is be a scrollable window like you'd see when viewing code on a blog site
-
-but to make it go off the grid we just don't specify it on the grid area, and we'll give it a different z-index
-2. as for mobile, that's something i haven't been keeping too close of an eye on. but it'd be pretty easy to re-order 
-the CSS for this site specifically for smaller screens. it isn't that complicated of a design to where there would be a 
-lot of overhead.
-also with respect to the preloader, I think I'm gonna re-work it entirely using hooks to make the nav and preloader 
-into the same component. the way it is right now definitely will be changing in the final stages of everything.
-
-Alright, so basically for the time being I should fiddle with this audio component to make sure it's actually working as intended?
-
-yeah I would say the next steps here would be adding an icon using fontawesome to the Audio component, and then
-making that icon have an onClick listener which would actually control the playing/pausing of the audio. If 
-that proves to not work so well, we can just give the audio tag a 'controls' attribute which will use the browser's 
-default media player (which isn't customizeable theme-wise, but it'll work and the rest of our stuff will be custom)
-
-Is the server running rn? I wanna see how this looks atm
-
-Lemme start it, we have an error with JSX tags in the Home.js file, lemme fix that fis
-
-Rodger that, couple things:
-
-1.) We have a grid, how exactly are we to make sure this track list goes where it needs to, we have it placed off to the side and I'm worried
-about getting it in that exact placement because I have no clue how to position that specifically within a grid.
-
-2.) Have you checked out our mobile view? Definitely something for later but as of right now the site isn't functional on mobile, 
-our nav bar is too large to fit the space and the actual pre-loader doesn't work too great with touch controls. 
-*/
+  return (
+    <div>
+      <video id={props.song.uri}>
+        <source type="video/mp4" src={props.url}></source>
+        Your browser does not support HTML5 video
+      </video>
+      <span>
+        {video ? (
+          <FontAwesomeIcon
+            icon={video.paused ? play : pause}
+            onClick={togglePlay}
+          />
+        ) : (
+          <FontAwesomeIcon icon={loading} />
+        )}
+        <p>{props.name}</p>
+      </span>
+    </div>
+  );
+};
